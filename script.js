0b47b0052f7453ffac2619b42ae9a1508172b00d
hergimerc <hzsn@hzsn.my.id>
2023-08-21 10:41:58 +0700

    188 smooth scrolling

--- script.js
+++ script.js
@@ -30,6 +30,13 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
 ////////////////////////////////////////////////////////////////////////////////////////////
 
 const header = document.querySelector('.header');
