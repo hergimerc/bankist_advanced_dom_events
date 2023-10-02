bbc038d7d40333417385efcfb08ee377e3e4d5f4
hergimerc <hzsn@hzsn.my.id>
2023-10-02 10:50:18 +0700

    193 dom traversing

--- script.js
+++ script.js
@@ -139,3 +139,21 @@ document.querySelector('.nav').addEventListener('click', function (e) {
   console.log(e.currentTarget === this);
 });
 */
+/*
+const h1 = document.querySelector('h1');
+
+// Going downwards: child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+// Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+console.log(h1.closest('.header'));
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+*/
